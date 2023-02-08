5c791a575ca98fc5673ab11f7cbee42725bd0501
Sajithmym <Sajithmym@gmail.com>
2023-02-08 11:38:31 +0530

    Check File Exist and open

--- Check File.bat
+++ Check File.bat
@@ -1,6 +1,13 @@
 @echo off
 title Check Files
 
-if exist "Input.bat" (echo Input.bat is available) else (echo file Not Available)
+if exist "Input.bat" (
+    echo Input.bat is available
+    echo Now opening Input.bat
+    echo.
+    "Input.bat"
+) else (
+    echo file Not Available
+)
 
 pause
