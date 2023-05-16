1576a5b7562f7794496ea9f5fb6deacf6745242d
Sajithmym <Sajithmym@gmail.com>
2023-05-16 16:11:34 +0530

    update

--- For Loop.bat
+++ For Loop.bat
@@ -5,7 +5,7 @@ for %%i in (5,6,7,8,9) do echo %%i
 pause
 cls
 
-for /l %%i in (0,50,2000) do echo Mohamed Sajith -- %%i -- 
+for /l %%i in (0,5,100) do echo Mohamed Sajith -- %%i -- 
 
 :execute
 echo %random%
---
+++ loop.bat
@@ -0,0 +1,5 @@
+@echo off
+title ForLoop
+
+for /l %%i in (1,1,10) do echo %%i
+pause
